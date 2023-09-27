4ece60fad3b8da15f18a8036b9b9462967ea6699
KrisYG <124745117+KrisYG@users.noreply.github.com>
2023-09-27 12:09:44 -0500

    Add files via upload
    Adding the Android Studio files for the app

---
+++ Strive/app/src/main/java/edu/utsa/cs3443/strive/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.utsa.cs3443.strive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
